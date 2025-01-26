6b36acf086f583f3a2b8f5d2472bd489646cd032
Boaz <webevolution13@gmail.com>
2025-01-26 20:29:09 +0100

    upDate
    front end

--- woonuxt_base/app/components/generalElements/AppHeader.vue
+++ woonuxt_base/app/components/generalElements/AppHeader.vue
@@ -23,12 +23,12 @@ const links = [
       </NuxtLink>
       <div class="lg:flex items-center hidden">
         <NuxtLink v-for="link in links" :key="link.name" :to="link.link"
-          class="text-sm w-[130px] flex h-[50px] items-center justify-center text-center transition-colors duration-200 hover:bg-light border-b-2 border-b-transparent hover:border-green box-border"
+          class="text-sm w-[150px] flex h-[50px] items-center justify-center text-center transition-colors duration-200 hover:bg-light border-b-2 border-b-transparent hover:border-green box-border"
           activeClass="bg-light border-b-2 !border-green">{{ link.name }}
         </NuxtLink>
       </div>
       <div class="flex gap-4">
-        <ProductSearch class="hidden md:inline-flex max-w-[320px]" />
+        <!-- <ProductSearch class="hidden md:inline-flex max-w-[320px]" /> -->
         <SignInLink />
         <CartTrigger />
       </div>
