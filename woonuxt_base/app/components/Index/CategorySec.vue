89b4ca192056754e098568af6503ea8036034907
Boaz <webevolution13@gmail.com>
2025-03-03 14:22:08 +0100

    upDate
    front end

--- woonuxt_base/app/components/Index/CategorySec.vue
+++ woonuxt_base/app/components/Index/CategorySec.vue
@@ -1,17 +1,20 @@
 <script lang="ts" setup>
-import { ProductsOrderByEnum } from '#woo';
-const { siteName, description, shortDescription, siteImage } = useAppConfig();
+const { data } = await useAsyncGql('getProductCategories');
+const productCategories = data.value.productCategories?.nodes as ProductCategory[];
 
-const { data } = await useAsyncGql('getProductCategories', { first: 8 });
-const productCategories = data.value?.productCategories?.nodes || [];
-
-
+useHead({
+    title: `Categories`,
+    meta: [{ name: 'description', content: 'All product categories' }],
+    link: [{ rel: 'canonical', href: 'https://v3.woonuxt.com/categories' }],
+});
 </script>
 
 <template>
-    <section class="container my-16">
-        <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-4">
-            <CategoryCard v-for="(category, i) in productCategories" :key="i" class="w-full" :node="category" />
+    <main class="container">
+        <div v-if="productCategories?.length"
+            class="grid grid-cols-2 gap-4 my-6 md:grid-cols-3 lg:gap-8 xl:grid-cols-4">
+            <CategoryCard v-for="(category, i) in productCategories" :key="i" :node="category"
+                :image-loading="i <= 2 ? 'eager' : 'lazy'" />
         </div>
-    </section>
+    </main>
 </template>
